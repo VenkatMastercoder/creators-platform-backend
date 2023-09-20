339fb9669e832713c86a0b852e009d0cedd19182
Venkatesan <venkatesangunaraj@gmail.com>
2023-09-20 17:23:46 +0530

    Added Cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import express, { Express, Application, Request, Response } from 'express';
+import cors from 'cors';
+
 const app: Application = express();
 const PORT: number = Number(process.env.PORT) || 5001;
-
 const prisma = new PrismaClient();
+
+app.use(cors());
 
 async function getAllUsers() {
   try {
@@ -15,10 +18,30 @@ async function getAllUsers() {
   }
 }
 
+
+async function getoneUsers(idd: string) {
+  try {
+    const users = await prisma.user.findUnique({
+      where: { id: idd },
+    });
+    return users;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
 app.use("/api/user",async (req: Request, res: Response) => {
   const data = await getAllUsers();
   res.status(200).json({ data });
 });
+
+app.get("/api/user/:id",async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = await getoneUsers(id);
+  res.status(200).json({ data });
+});
+
 
 app.get('/',async (req: Request, res: Response) => {
   res.send('This From Backend');
