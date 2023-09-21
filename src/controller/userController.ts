d6c03a72900a9afb76e7a51163dbd9504a4a802a
Venkatesan <venkatesangunaraj@gmail.com>
2023-09-21 20:10:16 +0530

    🐛 Updated userController

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -36,12 +36,12 @@ export const userIdUpdateController = async (req: Request, res: Response) => {
     const data = await userService.updateUsers(id, name, bio, image, socialMediaLinks, username);
 
     if (data.success) {
-      res.status(200).json({succes:data.success,message:"Data has been updated successfully."});
+      res.status(200).json({message:"Sucess"});
     } else {
       if (data.message === "Email is already in use." || data.message === "Username is already taken.") {
-        res.status(400).send(data.message);
+        res.status(400).json({message:data.message});
       } else {
-        res.status(500).send(data.message);
+        res.status(500).json({message:data.message});
       }
     }
   }
