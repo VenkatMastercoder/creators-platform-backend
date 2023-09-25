866f1e7b6df91637e2441f47e72cc2a26efe8b79
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-09-25 15:39:41 +0530

    Updated package.json for production

---
+++ dist/routes/prodRouter.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const prodController_1 = require("../controller/prodController");
+const router = express_1.default.Router();
+router.get("/", prodController_1.getAllDigitProdController);
+router.post("/add", prodController_1.postDigitProdController);
+router.delete("/:id", prodController_1.deleteDigitProdController);
+router.put("/:id", prodController_1.updateDigitProdController);
+exports.default = router;
---
+++ dist/server.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const userRouter_1 = __importDefault(require("./routes/userRouter"));
+// import fileRouter from "./routes/fileRouter";
+const prodRouter_1 = __importDefault(require("./routes/prodRouter"));
+const ErrorHandler_1 = require("./middleware/ErrorHandler");
+const app = (0, express_1.default)();
+const PORT = Number(process.env.PORT) || 5003;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/api/v1/users", userRouter_1.default);
+// app.use("/api/v1/file",fileRouter);
+app.use("/api/v1/digital_download", prodRouter_1.default);
+app.use(ErrorHandler_1.handleMulterError);
+app.get('/', async (req, res) => {
+    res.send('This From Backend');
+});
+app.listen(PORT, () => {
+    console.log('====================================');
+    console.log(`App listening on port ${PORT}`);
+    console.log('====================================');
+});
